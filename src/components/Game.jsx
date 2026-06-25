bed5aa6ef68bb2ed65501beb9ee61b771d52c015
klucky17 <kalynluc@gmail.com>
2026-06-25 00:58:07 -0600

    added cones, syrups, toppings

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,48 +2,84 @@ import { useState } from 'react'
 
 function Game({score, setScore}) {
 
-  // //get a randomized order
-  // function shuffleOrder(IceCreamOrder){
-  //   let currentIndex = IceCreamOrder.length
-  //   while(currentIndex != 0){
-  //     let randomIndex = Math.floor(Math.random() * currentIndex)
-  //     currentIndex--
-      
-  //     [IceCreamOrder[currentIndex], IceCreamOrder[randomIndex]] = [IceCreamOrder[randomIndex], IceCreamOrder[currentIndex]]
-  //   }
-  // }
-
   //get a random customer order for each new customer
   function getOrder(){
-    const totalFlavours = flavours.length  //get the total number of ice creams available to choose from
-    const orderLength = Math.floor(Math.random() * totalFlavours) + 1  //get random order length, minimum 1 scoop
-
     const order = []
-    for(let i=0; i<orderLength; i++){
+
+    //get random cone
+    const cone = cones[Math.floor(Math.random() * cones.length)]
+    order.push(cone)
+
+    //get random scoops
+    const totalFlavours = flavours.length  //get the total number of ice creams available to choose from
+    const numScoops = Math.floor(Math.random() * totalFlavours) + 1  //get random number of scoops, minimum 1 scoop
+    
+    for(let i=0; i<numScoops; i++){
       const randomFlavour = flavours[Math.floor(Math.random() * totalFlavours)]  //get random ice cream flavours
       order.push(randomFlavour)  //add flavour to customer order
+    }
+
+    //50% chance to get random syrups
+    if(Math.random() > 0.5){
+      const numSyrups = Math.floor(Math.random() * 2) + 1  //at most 2 syrups, minimum 1
+      for(let i=0; i<numSyrups; i++){
+        const randomSyrup = syrups[Math.floor(Math.random() * syrups.length)]
+        order.push(randomSyrup)
+      }
+    }
+
+    //50% chance to get random toppings
+    if(Math.random() > 0.5){
+      const numToppings = Math.floor(Math.random() * 2) + 1  //at most 2 syrups, minimum 1
+      for(let i=0; i<numToppings; i++){
+        const randomTopping = toppings[Math.floor(Math.random() * toppings.length)]
+        order.push(randomTopping)
+      }
     }
     
     return order
   }
 
-  function addFlavour(item){
+  function addIceCream(item){
     setIceCream([...playerIceCream, item])
+  }
+  function addCone(item){
+    setCone(item)
+  }
+  function addSyrup(item){
+    setSyrup([...playerSyrup, item])
+  }
+  function addTopping(item){
+    setTopping([...playerTopping, item])
+  }
+
+  function resetPlayerOrder(){
+    setIceCream([])
+    setCone(null)
+    setSyrup([])
+    setTopping([])
   }
 
   //check if created ice cream matches customer order
   function checkOrder(){
-    const correct = JSON.stringify(order) === JSON.stringify(playerIceCream) //compare orders
-    if (correct){
-      setScore(score + 100)
-      setIceCream([])  //reset ice cream creation
+    const correct = JSON.stringify(order) === JSON.stringify(playerOrder) //compare orders
+    if(correct){
+      setScore(score + 25*(order.length-1))  //25 points for each item excluding the cone
+      resetPlayerOrder()  //reset created order
       setOrder(getOrder())  //get new customer order
     }
   }
 
-  const flavours = ["Vanilla", "Strawberry", "Chocolate", "Mint", "Ube", "Orange", "Cotton Candy", "Cherry", "Lemon"]  //all ice cream flavours
-  const [order, setOrder] = useState(getOrder(flavours))
+  const flavours = ["Vanilla", "Strawberry", "Chocolate", "Mint", "Ube", "Orange", "Bubble Gum", "Tomato", "Lemon"]  //all ice cream flavours
+  const cones = ["Cup", "Waffle"]
+  const syrups = ["Chocolate Syrup", "Strawberry Syrup", "Caramel Syrup"]
+  const toppings = ["Cherries", "Sprinkles", "Marshmallows"]
+  const [order, setOrder] = useState(getOrder())
   const [playerIceCream, setIceCream] = useState([])
+  const [playerCone, setCone] = useState(null)  //can only have 1 cone at a time
+  const [playerSyrup, setSyrup] = useState([])
+  const [playerTopping, setTopping] = useState([])
+  const playerOrder = [playerCone, ...playerIceCream, ...playerSyrup, ...playerTopping]  //combine everything into 1 array
 
   return(
     /*display current score*/
@@ -56,7 +92,7 @@ function Game({score, setScore}) {
 
       {/*display order from the array*/}
         <ul>
-          {order.map((item, index) => (
+          {order.slice().reverse().map((item, index) => (  //reverse order so that cones are that the bottom, toppings at the top
             <p key={index}>{item}</p>
           ))}
         </ul>
@@ -64,31 +100,62 @@ function Game({score, setScore}) {
 
       {/*display all flavours as buttons*/}
       <div className="buttons">
+
+        {toppings.map((topping) => (
+          <button
+            key={topping}
+            onClick={() => addTopping(topping)}  /*add scoop*/
+          >
+            {topping}
+          </button>
+        ))}
+
+        {syrups.map((syrup) => (
+          <button
+            key={syrup}
+            onClick={() => addSyrup(syrup)}  /*add scoop*/
+          >
+            {syrup}
+          </button>
+        ))}
+
         {flavours.map((flavour) => (
           <button
             key={flavour}
-            onClick={() => addFlavour(flavour)}  /*add scoop*/
+            onClick={() => addIceCream(flavour)}  /*add scoop*/
           >
             {flavour}
           </button>
         ))}
+
+        {cones.map((cone) => (
+          <button
+            key={cone}
+            onClick={() => addCone(cone)}  /*add scoop*/
+          >
+            {cone}
+          </button>
+        ))}
+
       </div>
 
       <div className="player-order">
-        <h3>Your Ice Cream:</h3>
+        <h3>Your Order:</h3>
 
         <ul>
-          {playerIceCream.map((item, index) => (
-            <li key={index}>{item}</li>
+          {playerOrder.filter(Boolean).slice().reverse().map((item, index) => (
+            <p key={index}>{item}</p>
           ))}
         </ul>
       </div>
 
+      {/*check order and serve to customer*/}
       <button onClick={checkOrder}>
         Serve
       </button>
 
-      <button onClick={() => setIceCream([])}>
+      {/*reset ice cream*/}
+      <button onClick={resetPlayerOrder}>
         Garbage
       </button>
     </div>
